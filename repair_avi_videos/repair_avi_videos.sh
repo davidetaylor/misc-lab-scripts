59fea356ca6189f09a56c8b15ad81e807cbeabd8
davidetaylor <davidt496@gmail.com>
2023-11-03 16:48:02 -0700

    add missing code

--- repair_avi_videos/repair_avi_videos.sh
+++ repair_avi_videos/repair_avi_videos.sh
@@ -1,1 +1,22 @@
 
+#!/bin/bash
+
+# Directory containing the AVI files
+DIR="./"
+
+# Ensure the "original_videos" directory exists
+mkdir -p "$DIR/original_videos"
+
+# Iterate over every AVI file in the directory
+for file in "$DIR"*.avi; do
+    # Use FFmpeg to repair the file
+    ffmpeg -i "$file" -c:v copy -an "${file%.avi}_fixed.avi"
+    
+    # Move the original to "original_videos" directory
+    mv "$file" "$DIR/original_videos/"
+
+    # Rename the fixed video to the original name
+    mv "${file%.avi}_fixed.avi" "$file"
+    
+    echo "Processed $file"
+done
